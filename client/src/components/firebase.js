45e10ebc743f1ff8f01711b5a1835c3ccecdb426
Abhay Singh Chauhan <abhaysinghchauhanasc01234@gmail.com>
2025-11-29 14:16:19 +0530

    Security: Move Firebase config to environment variables

--- client/src/components/firebase.js
+++ client/src/components/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,12 +7,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBXt7DGOUSZx0FyODkQv0pyIGc_ten2iFM",
-    authDomain: "hms-services.firebaseapp.com",
-    projectId: "hms-services",
-    storageBucket: "hms-services.firebasestorage.app",
-    messagingSenderId: "643923980378",
-    appId: "1:643923980378:web:f7e3dce23bec2d6c3f2863"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
